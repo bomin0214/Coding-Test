1e4fdf96690cadfc04931244c580e457ab1e5b1c
bomikim0214 <rlaqhals05@naver.com>
2026-03-15 21:43:09 +0900

    2026.3.15

---
+++ src/B1920.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class B1920 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        HashSet<Integer> hashset = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            int value = Integer.parseInt(st.nextToken());
+            hashset.add(value);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+
+            if (hashset.contains(target)) {
+                sb.append(1).append("\n");
+            } else {
+                sb.append(0).append("\n");
+            }
+        }
+
+        System.out.print(sb.toString());
+    }
+}
