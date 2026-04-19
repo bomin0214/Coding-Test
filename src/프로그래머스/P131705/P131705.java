55751886151a3a5f2893e182d8f93add3ef71968
bomikim0214 <rlaqhals05@naver.com>
2026-04-19 17:26:27 +0900

    2026. 04. 19 P131705

---
+++ src/프로그래머스/P131705/P131705.java
@@ -0,0 +1,19 @@
+package 프로그래머스.P131705;
+
+class P131705 {
+    public int solution(int[] number) {
+        int answer = 0;
+
+        for (int i = 0; i < number.length - 2; i++) {
+            for (int j = i + 1; j < number.length - 1; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
